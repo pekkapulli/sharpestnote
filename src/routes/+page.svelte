5a939e7559a0dbffccf84d2dc4885a937a624624
Pekka Pulli <pekka.pulli@gmail.com>
2025-12-19 11:49:45 +0200

    px

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import TheSharpestNoteLogo from '../lib/assets/The Sharpest Note Logo.svg';
 </script>
 
-<div class="py-2rem flex min-h-screen flex-col items-center justify-center bg-off-white">
+<div class="py-2rem flex min-h-screen flex-col items-center justify-center bg-off-white px-1">
 	<img src={TheSharpestNoteLogo} alt="The Sharpest Note" class="mb-8 w-200" />
 	<h2 class="mb-4 text-center text-4xl font-light">Enhancing music education for curious ears</h2>
 	<p class="text-center">Coming!</p>
