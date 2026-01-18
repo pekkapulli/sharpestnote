b789654eacbdb67e612abba664de763fc79dca48
Pekka Pulli <pekka.pulli@gmail.com>
2026-01-18 15:42:42 +0200

    Hämä hämä häkki

--- src/lib/config/units.ts
+++ src/lib/config/units.ts
@@ -798,6 +798,92 @@ export const units: Units = {
 						{ note: 'd/4', length: 8, finger: 0, string: 'Dad' }
 					]
 				]
+			},
+			{
+				code: 'hämähämähäkki',
+				label: 'Hämä hämä häkki',
+				composer: 'trad.',
+				arranger: 'trad.',
+				notationStartPercent: 0.18,
+				notationEndPercent: 0.9,
+				key: 'D',
+				mode: 'major',
+				barLength: 16,
+				scale: [
+					{ note: 'd/4', length: 4, finger: 0, string: 'Dad' },
+					{ note: 'e/4', length: 4, finger: 1, string: 'Dad' },
+					{ note: 'f#/4', length: 4, finger: 2, string: 'Dad' },
+					{ note: 'g/4', length: 4, finger: 3, string: 'Dad' },
+					{ note: 'a/4', length: 4, finger: 0, string: 'Mom' },
+					{ note: 'b/4', length: 4, finger: 1, string: 'Mom' },
+					{ note: 'c#/5', length: 4, finger: 2, string: 'Mom' },
+					{ note: 'd/5', length: 4, finger: 3, string: 'Mom' }
+				],
+				melody: [
+					[
+						{ note: 'd/4', length: 4, finger: 0, string: 'Dad' },
+						{ note: 'd/4', length: 4, finger: 0, string: 'Dad' },
+						{ note: 'd/4', length: 4, finger: 0, string: 'Dad' },
+						{ note: 'e/4', length: 4, finger: 1, string: 'Dad' },
+						{ note: 'f#/4', length: 8, finger: 2, string: 'Dad' },
+						{ note: 'f#/4', length: 8, finger: 2, string: 'Dad' }
+					],
+					[
+						{ note: 'e/4', length: 4, finger: 1, string: 'Dad' },
+						{ note: 'd/4', length: 4, finger: 0, string: 'Dad' },
+						{ note: 'e/4', length: 4, finger: 1, string: 'Dad' },
+						{ note: 'f#/4', length: 4, finger: 2, string: 'Dad' },
+						{ note: 'd/4', length: 8, finger: 0, string: 'Dad' },
+						{ note: null, length: 8 }
+					],
+					[
+						{ note: 'f#/4', length: 4, finger: 2, string: 'Dad' },
+						{ note: 'f#/4', length: 4, finger: 2, string: 'Dad' },
+						{ note: 'f#/4', length: 4, finger: 2, string: 'Dad' },
+						{ note: 'g/4', length: 4, finger: 3, string: 'Dad' },
+						{ note: 'a/4', length: 8, finger: 0, string: 'Mom' },
+						{ note: 'a/4', length: 8, finger: 0, string: 'Mom' }
+					],
+					[
+						{ note: 'g/4', length: 4, finger: 3, string: 'Dad' },
+						{ note: 'f#/4', length: 4, finger: 2, string: 'Dad' },
+						{ note: 'g/4', length: 4, finger: 3, string: 'Dad' },
+						{ note: 'a/4', length: 4, finger: 0, string: 'Mom' },
+						{ note: 'f#/4', length: 8, finger: 2, string: 'Dad' },
+						{ note: null, length: 8 }
+					],
+					[
+						{ note: 'd/5', length: 8, finger: 3, string: 'Mom' },
+						{ note: 'd/5', length: 4, finger: 3, string: 'Mom' },
+						{ note: 'd/5', length: 4, finger: 3, string: 'Mom' },
+						{ note: 'c#/5', length: 8, finger: 2, string: 'Mom' },
+						{ note: 'c#/5', length: 8, finger: 2, string: 'Mom' }
+					],
+					[
+						{ note: 'b/4', length: 4, finger: 1, string: 'Mom' },
+						{ note: 'b/4', length: 4, finger: 1, string: 'Mom' },
+						{ note: 'b/4', length: 4, finger: 1, string: 'Mom' },
+						{ note: 'b/4', length: 4, finger: 1, string: 'Mom' },
+						{ note: 'a/4', length: 8, finger: 0, string: 'Mom' },
+						{ note: null, length: 8 }
+					],
+					[
+						{ note: 'd/4', length: 4, finger: 0, string: 'Dad' },
+						{ note: 'd/4', length: 4, finger: 0, string: 'Dad' },
+						{ note: 'd/4', length: 4, finger: 0, string: 'Dad' },
+						{ note: 'e/4', length: 4, finger: 1, string: 'Dad' },
+						{ note: 'f#/4', length: 8, finger: 2, string: 'Dad' },
+						{ note: 'f#/4', length: 8, finger: 2, string: 'Dad' }
+					],
+					[
+						{ note: 'e/4', length: 4, finger: 1, string: 'Dad' },
+						{ note: 'd/4', length: 4, finger: 0, string: 'Dad' },
+						{ note: 'e/4', length: 4, finger: 1, string: 'Dad' },
+						{ note: 'f#/4', length: 4, finger: 2, string: 'Dad' },
+						{ note: 'd/4', length: 8, finger: 0, string: 'Dad' },
+						{ note: null, length: 8 }
+					]
+				]
 			}
 		]
 	}
