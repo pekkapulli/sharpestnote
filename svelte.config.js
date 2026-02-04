5c9a2a7055842334662e2beb336b9f8256599c65
Pekka Pulli <pekka.pulli@gmail.com>
2026-02-04 19:20:38 +0200

    Try again

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,7 @@ const config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
-	kit: { adapter: adapter() },
+	kit: { adapter: adapter({ compatibilityFlags: ['nodejs_compat'] }) },
 	compilerOptions: {
 		runes: true
 	}
